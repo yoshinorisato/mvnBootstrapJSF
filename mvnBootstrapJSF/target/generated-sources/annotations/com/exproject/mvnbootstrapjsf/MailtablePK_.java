8f3aa6124f852f7a1be8f47044ef91d10d89895a
yoshinorisato <satoyoshi22@gmail.com>
2015-09-14 09:11:49 +0900

    modified:   mvnBootstrapJSF/target/generated-sources/annotations/com/exproject/mvnbootstrapjsf/MailtablePK_.java 	modified:   mvnBootstrapJSF/target/generated-sources/annotations/com/exproject/mvnbootstrapjsf/Mailtable_.java 	modified:   mvnBootstrapJSF/target/maven-archiver/pom.properties 	modified:   mvnBootstrapJSF/target/mvnBootstrapJSF-1.0-SNAPSHOT.war 	modified:   mvnBootstrapJSF/target/mvnBootstrapJSF-1.0-SNAPSHOT/WEB-INF/classes/com/exproject/mvnbootstrapjsf/DBaccess.class 	modified:   mvnBootstrapJSF/target/mvnBootstrapJSF-1.0-SNAPSHOT/WEB-INF/classes/com/exproject/mvnbootstrapjsf/Mailtable.class 	modified:   mvnBootstrapJSF/target/mvnBootstrapJSF-1.0-SNAPSHOT/WEB-INF/classes/com/exproject/mvnbootstrapjsf/MailtablePK.class 	modified:   mvnBootstrapJSF/target/mvnBootstrapJSF-1.0-SNAPSHOT/WEB-INF/classes/com/exproject/mvnbootstrapjsf/MailtablePK_.class 	modified:   mvnBootstrapJSF/target/mvnBootstrapJSF-1.0-SNAPSHOT/WEB-INF/classes/com/exproject/mvnbootstrapjsf/Mailtable_.class 	modified:   mvnBootstrapJSF/target/mvnBootstrapJSF-1.0-SNAPSHOT/WEB-INF/classes/com/exproject/mvnbootstrapjsf/NewWSEndpoint.class 	modified:   mvnBootstrapJSF/target/mvnBootstrapJSF-1.0-SNAPSHOT/WEB-INF/classes/com/exproject/mvnbootstrapjsf/calendarView.class 	modified:   mvnBootstrapJSF/target/mvnBootstrapJSF-1.0-SNAPSHOT/WEB-INF/classes/com/exproject/mvnbootstrapjsf/mailTableModel.class

--- mvnBootstrapJSF/target/generated-sources/annotations/com/exproject/mvnbootstrapjsf/MailtablePK_.java
+++ mvnBootstrapJSF/target/generated-sources/annotations/com/exproject/mvnbootstrapjsf/MailtablePK_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-09-10T13:50:03")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-09-11T09:27:48")
 @StaticMetamodel(MailtablePK.class)
 public class MailtablePK_ { 
 
